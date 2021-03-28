b6ea5600452ad93812c51ecb54418c4b1ffce1d5
Abestanis <abestanis.gc@gmail.com>
2021-03-29 01:13:17 +0200

    Add an info label if no passwords are found for an embedded website

--- src/classes/CredentialsDropdown.ts
+++ src/classes/CredentialsDropdown.ts
@@ -213,6 +213,23 @@ export default class CredentialsDropdown {
         } else { // No credentials available
             this._credentialItems = undefined;
             container.empty().append($('<div>').addClass(styles.noResults).text('No credentials found'));
+            if (self != top) {
+                const iframeInfo = $('<div>').addClass(styles.iframeInfo);
+                iframeInfo.append($('<div>').text(
+                    'This input is part of a website that is embedded into the current website. ' +
+                    'Passwords are searched for the url of the embedded website.'));
+                // noinspection HtmlRequiredAltAttribute,RequiredAttributes
+                const copyToClipboardIcon = $('<img>')
+                    .attr('alt', 'Copy url').attr('title', 'Copy url')
+                    .attr('src', chrome.extension.getURL('images/copy_to_clipboard.svg'))
+                    .attr('tabindex', '0').addClass(styles.copyUrlIcon);
+                iframeInfo.append($('<div>').append($('<label>').addClass(styles.copyUrlIconLabel).text(
+                    'Copy url').append(copyToClipboardIcon.on('click', (event) => {
+                    event.preventDefault();
+                    this._copyIframeUrl(copyToClipboardIcon)
+                }))));
+                container.append(iframeInfo);
+            }
         }
     }
 
@@ -220,6 +237,23 @@ export default class CredentialsDropdown {
     private _openOptionsWindow() {
         Client.openOptions();
         this.close();
+    }
+
+    /**
+     * Copy the url of the current iframe into the clipboard.
+     * @param icon The icon that was clicked.
+     */
+    private _copyIframeUrl(icon: JQuery) {
+        const $temp = $("<input type='url'>");
+        $(document.body).append($temp);
+        $temp.val(window.location.href).trigger('select');
+        const success = document.execCommand('copy');
+        $temp.remove();
+        if (success) {
+            icon.attr('src', chrome.extension.getURL('images/copy_to_clipboard_success.svg'));
+            setTimeout(() => icon.attr('src', chrome.extension.getURL('images/copy_to_clipboard.svg')), 3000);
+        }
+        this._fieldSet?.controlField?.trigger('focus');
     }
 
     /** Handle a click on a credential field. */
